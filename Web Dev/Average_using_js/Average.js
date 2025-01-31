07f28a73da276dbca3d01195083d43692bd675c2
LukeWomack <114890724+LukeWomack@users.noreply.github.com>
2025-01-31 16:49:26 -0600

    Add files via upload

---
+++ Web Dev/Average_using_js/Average.js
@@ -0,0 +1,9 @@
+// defining avg func
+function avg(a, b){
+    return (a + b) / 2;
+}
+
+// initializing a + b as 100 and 300
+const average = avg(100, 300)
+// outputting result to console
+console.log(average);
