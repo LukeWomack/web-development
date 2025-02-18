fb4545a9795836aafc6074536c279be85b9c1101
LukeWomack <114890724+LukeWomack@users.noreply.github.com>
2025-02-17 21:29:27 -0600

    code along assignment 8

---
+++ code_along_8/script.js
@@ -0,0 +1,14 @@
++document.getElementById("fetchUsers").addEventListener("click", function (){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => {
+        let userList = document.getElementById("userList");
+        userList.innterHTML='';
+        data.forEach(user => {
+            let li = document.createElement("li")
+            li.innerHTML= `<strong> ${user.name}</strong> - ${user.email}`;
+            userList.appendChild(li);
+        });
+    })
+    .catch(error => console.error("Error fetching data: ", error));
+});
